84bddec1d7dbd03024e1ca0735e981cbadd5e267
nathija-nimantha <nathija.max99@gmail.com>
2024-06-19 10:10:21 +0530

    third commit

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-function clac(){
+function calc() {
     let num1 = document.getElementById('txtNumber1').value;
     let operator = document.getElementById('txtoperator').value;
     let num2 = document.getElementById('txtNumber2').value;
-    let lblOuput = document.getElementById('lblOuput');
+    let lblOutput = document.getElementById('lblOutput');
 
     num1 = Number(num1);
-    operator = Number(operator);
     num2 = Number(num2);
 
-    if(operator == '+'){
-        lblOuput.innerHTML = num1 + num2;
-    } else if(operator == '-'){
-        lblOuput.innerHTML = num1 - num2;
-    } else if(operator == '*'){
-        lblOuput.innerHTML = num1 * num2;
-    } else if(operator == '/'){
-        lblOuput.innerHTML = num1 / num2;
-    } else if(operator == '%'){
-        lblOuput.innerHTML = num1 % num2;
+    switch (operator) {
+        case "+":
+            lblOutput.innerHTML = num1 + num2;
+            break;
+        case "-":
+            lblOutput.innerHTML = num1 - num2;
+            break;
+        case "*":
+            lblOutput.innerHTML = num1 * num2;
+            break;
+        case "/":
+            lblOutput.innerHTML = num1 / num2;
+            break;
+        default:
+            lblOutput.innerHTML = "Invalid operator";
+            break;
     }
 }
-    document.getElementById('btnCalc').addEventListener('click', clac);
+
+document.getElementById('btnCalc').addEventListener('click', calc);
