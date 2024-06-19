526386256efa46754cab3fb79f584ab1e87ca7db
nathija-nimantha <nathija.max99@gmail.com>
2024-06-19 09:51:08 +0530

    added operators field

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 function clac(){
     let num1 = document.getElementById('txtNumber1').value;
+    let operator = document.getElementById('txtoperator').value;
     let num2 = document.getElementById('txtNumber2').value;
     let lblOuput = document.getElementById('lblOuput');
 
-    console.log(typeof num1);
-    console.log(typeof num2);
-
     num1 = Number(num1);
+    operator = Number(operator);
     num2 = Number(num2);
-    lblOuput.innerHTML = num1 + num2;
-}
 
-document.getElementById('btnCalc').addEventListener('click', clac);
+    if(operator == '+'){
+        lblOuput.innerHTML = num1 + num2;
+    } else if(operator == '-'){
+        lblOuput.innerHTML = num1 - num2;
+    } else if(operator == '*'){
+        lblOuput.innerHTML = num1 * num2;
+    } else if(operator == '/'){
+        lblOuput.innerHTML = num1 / num2;
+    } else if(operator == '%'){
+        lblOuput.innerHTML = num1 % num2;
+    }
+}
+    document.getElementById('btnCalc').addEventListener('click', clac);
