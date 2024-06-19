2ba71c101705ab045c69be3d41d32da3a31258cb
nathija-nimantha <nathija.max99@gmail.com>
2024-06-19 13:22:50 +0530

    Forth commit

--- app.js
+++ app.js
@@ -1,29 +1,72 @@
-function calc() {
-    let num1 = document.getElementById('txtNumber1').value;
-    let operator = document.getElementById('txtoperator').value;
-    let num2 = document.getElementById('txtNumber2').value;
-    let lblOutput = document.getElementById('lblOutput');
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let result = '';
 
-    num1 = Number(num1);
-    num2 = Number(num2);
-
-    switch (operator) {
-        case "+":
-            lblOutput.innerHTML = num1 + num2;
-            break;
-        case "-":
-            lblOutput.innerHTML = num1 - num2;
-            break;
-        case "*":
-            lblOutput.innerHTML = num1 * num2;
-            break;
-        case "/":
-            lblOutput.innerHTML = num1 / num2;
-            break;
-        default:
-            lblOutput.innerHTML = "Invalid operator";
-            break;
-    }
+function updateDisplay() {
+    document.getElementById('fNum').innerText = firstNumber;
+    document.getElementById('lblOperator').innerText = operator;
+    document.getElementById('sNum').innerText = secondNumber;
+    document.getElementById('lblOutput').innerText = result;
 }
 
-document.getElementById('btnCalc').addEventListener('click', calc);
+function numberButtonClick(value) {
+    if (operator) {
+        secondNumber += value;
+    } else {
+        firstNumber += value;
+    }
+    updateDisplay();
+}
+
+function operatorButtonClick(value) {
+    if (firstNumber && !secondNumber) {
+        operator = value;
+    }
+    updateDisplay();
+}
+
+function calculate() {
+    let num1 = parseFloat(firstNumber);
+    let num2 = parseFloat(secondNumber);
+
+    if (operator === '+') {
+        result = num1 + num2;
+    } else if (operator === '-') {
+        result = num1 - num2;
+    } else if (operator === '*') {
+        result = num1 * num2;
+    } else if (operator === '/') {
+        if (num2 !== 0) {
+            result = num1 / num2;
+        } else {
+            result = 'Error';
+        }
+    }
+    updateDisplay();
+}
+
+function clearAll() {
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    result = '';
+    updateDisplay();
+}
+
+document.getElementById('btn0').addEventListener('click', () => numberButtonClick('0'));
+document.getElementById('btn1').addEventListener('click', () => numberButtonClick('1'));
+document.getElementById('btn2').addEventListener('click', () => numberButtonClick('2'));
+document.getElementById('btn3').addEventListener('click', () => numberButtonClick('3'));
+document.getElementById('btn4').addEventListener('click', () => numberButtonClick('4'));
+document.getElementById('btn5').addEventListener('click', () => numberButtonClick('5'));
+document.getElementById('btn6').addEventListener('click', () => numberButtonClick('6'));
+document.getElementById('btn7').addEventListener('click', () => numberButtonClick('7'));
+document.getElementById('btn8').addEventListener('click', () => numberButtonClick('8'));
+document.getElementById('btn9').addEventListener('click', () => numberButtonClick('9'));
+document.getElementById('btnAdd').addEventListener('click', () => operatorButtonClick('+'));
+document.getElementById('btnSub').addEventListener('click', () => operatorButtonClick('-'));
+document.getElementById('btnMul').addEventListener('click', () => operatorButtonClick('*'));
+document.getElementById('btnDiv').addEventListener('click', () => operatorButtonClick('/'));
+document.getElementById('btnCalc').addEventListener('click', calculate);
+document.getElementById('btnDel').addEventListener('click', clearAll);
